139dbbe2c9f2feb4c5de3bdae072970b2a8f78e2
Techon-info <techoninfotech2024@gmail.com>
2025-09-23 20:38:18 +0530

    Update App.jsx service route update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React,{useState,useEffect} from 'react'
-import {Routes,Route} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contentcreation from './pages/Contentcreation';
@@ -12,8 +12,14 @@ import Contact from './pages/Contact';
 import Scroll from './pages/Scroll';
 import Loader from './Loader';
 
+// // ✅ Newly added imports
+ import Socialmediamarketing from './pages/SocialMediaMarketing';
+ import Searchengine from './pages/SearchEngine';
+ import Contentwriting from './pages/ContentWriting';
+  import Videography from './pages/Videography';
+ import Recuritintern from './pages/Recuritintern';
 const App = () => {
-   const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const handleLoad = () => {
@@ -36,23 +42,29 @@ const App = () => {
   if (isLoading) {
     return <Loader />;
   }
+
   return (
     <div>
-      <Scroll/>
+      <Scroll />
       <Routes>
-
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contentcreation' element={<Contentcreation/>} />
-        <Route path='/poster' element={<Posterdesign/>}/>
-        <Route path='/seo' element={<Seo/>}/>
-        <Route path='/socialmedia' element={<Socialmediamanagement/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contentcreation" element={<Contentcreation />} />
+        <Route path="/poster" element={<Posterdesign />} />
+        <Route path="/seo" element={<Seo />} />
+        <Route path="/socialmedia" element={<Socialmediamanagement />} />
         <Route path="/video-editing" element={<Video />} />
         <Route path="/email-marketing" element={<Email />} />
         <Route path="/contact" element={<Contact />} />
+         <Route path="/content-writing" element={<Contentwriting />} />
+         <Route path="/media-marketing" element={<Socialmediamarketing />} />
+         <Route path="/search-marketing" element={<Searchengine />} />
+           <Route path="/video" element={<Videography />} /> 
+            <Route path="/ri" element={<Recuritintern />} /> 
+
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
